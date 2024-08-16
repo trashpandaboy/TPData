6f1d9f7f2b69906b361a3088a67234591a625752
William Soro <william.soro@hotmail.it>
2024-08-17 00:34:13 +0200

    perf: DefaultValue method is now less complex

--- Runtime/DataCollection.cs
+++ Runtime/DataCollection.cs
@@ -67,67 +67,22 @@ namespace com.trashpandaboy.datastructures
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public virtual T GetDefaultValue<T>()
-        {
+        {            
             T defaultValue;
-            switch (Type.GetTypeCode(typeof(T)))
+            
+            if (typeof(T).IsValueType)
             {
-                case TypeCode.Boolean:
-                    defaultValue = (T)(object)false;
-                    break;
-                case TypeCode.Byte:
-                    defaultValue = (T)(object)(byte)0;
-                    break;
-                case TypeCode.Int32:
-                    defaultValue = (T)(object)0;
-                    break;
-                case TypeCode.String:
-                    defaultValue = (T)(object)String.Empty;
-                    break;
-                case TypeCode.Char:
-                    defaultValue = (T)(object)(char)0;
-                    break;
-                case TypeCode.DateTime:
-                    defaultValue = (T)(object)DateTime.MinValue;
-                    break;
-                //case TypeCode.DBNull:
-                //    break;
-                case TypeCode.Decimal:
-                    defaultValue = (T)(object)(decimal)0;
-                    break;
-                case TypeCode.Double:
-                    defaultValue = (T)(object)0d;
-                    break;
-                case TypeCode.Empty:
-                    defaultValue = (T)(object)null;
-                    break;
-                case TypeCode.Int16:
-                    defaultValue = (T)(object)(Int16)0;
-                    break;
-                case TypeCode.Int64:
-                    defaultValue = (T)(object)(Int64)0;
-                    break;
-                case TypeCode.Object:
-                    defaultValue = (T)(object)null;
-                    break;
-                case TypeCode.SByte:
-                    defaultValue = (T)(object)(byte)0;
-                    break;
-                case TypeCode.Single:
-                    defaultValue = (T)(object)0f;
-                    break;
-                case TypeCode.UInt16:
-                    defaultValue = (T)(object)(UInt16)0;
-                    break;
-                case TypeCode.UInt32:
-                    defaultValue = (T)(object)(UInt32)0;
-                    break;
-                case TypeCode.UInt64:
-                    defaultValue = (T)(object)(UInt64)0;
-                    break;
-                default:
-                    defaultValue = (T)(object)null;
-                    break;
+                defaultValue = default(T);
             }
+            else if(typeof(T) == typeof(string))
+            {
+                defaultValue = (T)(object)string.Empty;
+            }
+            else
+            {
+                defaultValue = (T)(object)null;
+            }
+
             return defaultValue;
         }
     }
